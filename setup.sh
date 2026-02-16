9829fb489a82b6bf29cc7f43288a41cced5d996c
chengq220 <chengqiu220@gmail.com>
2026-02-15 20:55:04 -0500

    update setup

--- setup.sh
+++ setup.sh
@@ -2,13 +2,18 @@
 
 pip install gdown
 
-python setup.py install
+SAM2_BUILD_ALLOW_ERRORS=0 pip install -v -e ".[notebooks]"
 
 mkdir datasets
 cd datasets
+mkdir endovis2017
+cd endovis2017
 gdown --fuzzy "https://drive.google.com/file/d/1dL7s4iaC1JWxhxf3V3iBSr9cUxxZaaR8/view?usp=sharing"
-gdown --fuzzy "https://drive.google.com/file/d/1B7QYCG5JwtWTneq2WSqjPIbl04NQdEmv/view?usp=sharing"
 tar -xvf "eval_endovis2017_surgical.tar"
+cd ..
+mkdir endovis2018
+cd endovis2018
+gdown --fuzzy "https://drive.google.com/file/d/1B7QYCG5JwtWTneq2WSqjPIbl04NQdEmv/view?usp=sharing"
 tar -xvf "eval_endovis18_surgical.tar"
 cd ..
 
