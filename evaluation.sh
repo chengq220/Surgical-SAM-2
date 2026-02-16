c9a60ce2a826c9d55071e3a0e3e82deff6a38af1
chengq220 <chengqiu220@gmail.com>
2026-02-15 21:35:51 -0500

    do stuff

--- evaluation.sh
+++ evaluation.sh
@@ -2,7 +2,7 @@ python tools/vos_inference.py \
     --sam2_cfg configs/sam2.1/sam2.1_hiera_s.yaml \
     --sam2_checkpoint ./checkpoints/sam2.1_hiera_s_endo18.pth \
     --output_mask_dir ./results/sam2.1/endovis_2018/instrument \
-    --input_mask_dir ./datasets/VOS-Endovis18/valid/VOS/Annotations_vos_instrument \
-    --base_video_dir ./datasets/VOS-Endovis18/valid/JPEGImages \
-    --gt_root ./datasets/VOS-Endovis18/valid/Annotations \
+    --input_mask_dir ./datasets/endovis2018/valid/instrument_annot \
+    --base_video_dir ./datasets/endovis2018/valid/JPEGImages \
+    --gt_root ./datasets/endovis2018/valid/surgical_Annotations \
     --gpu_id 0
