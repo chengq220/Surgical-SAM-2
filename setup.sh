6a24b39599374bba20a703f518fc3d36e8aa6871
chengq220 <chengqiu220@gmail.com>
2026-02-15 19:53:53 -0500

    update

--- setup.sh
+++ setup.sh
@@ -1,13 +1,16 @@
+#!/usr/bin/env bash
+
 pip install gdown
 
-python setup.py
+python setup.py install
 
 mkdir datasets
-cd datsets
+cd datasets
 gdown --fuzzy "https://drive.google.com/file/d/1dL7s4iaC1JWxhxf3V3iBSr9cUxxZaaR8/view?usp=sharing"
 gdown --fuzzy "https://drive.google.com/file/d/1B7QYCG5JwtWTneq2WSqjPIbl04NQdEmv/view?usp=sharing"
+tar -xvf "eval_endovis2017_surgical.tar"
+tar -xvf "eval_endovis18_surgical.tar"
 cd ..
 
 cd checkpoints
 gdown --fuzzy "https://drive.google.com/file/d/1DyrrLKst1ZQwkgKM7BWCCwLxSXAgOcMI/view?usp=sharing"
-
