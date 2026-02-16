c075af3c0469d6b22a09cbcf1db49f1b29c764ec
chengq220 <chengqiu220@gmail.com>
2026-02-15 19:43:32 -0500

    update

---
+++ setup.sh
@@ -0,0 +1,13 @@
+pip install gdown
+
+python setup.py
+
+mkdir datasets
+cd datsets
+gdown --fuzzy "https://drive.google.com/file/d/1dL7s4iaC1JWxhxf3V3iBSr9cUxxZaaR8/view?usp=sharing"
+gdown --fuzzy "https://drive.google.com/file/d/1B7QYCG5JwtWTneq2WSqjPIbl04NQdEmv/view?usp=sharing"
+cd ..
+
+cd checkpoints
+gdown --fuzzy "https://drive.google.com/file/d/1DyrrLKst1ZQwkgKM7BWCCwLxSXAgOcMI/view?usp=sharing"
+
