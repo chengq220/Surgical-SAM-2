bcca975cb806fc2c5b3c9f28664710ea884c498b
chengq220 <chengqiu220@gmail.com>
2026-02-16 10:18:24 -0500

    updte

--- setup.sh
+++ setup.sh
@@ -7,16 +7,20 @@ SAM2_BUILD_ALLOW_ERRORS=0 pip install -v -e ".[notebooks]"
 mkdir datasets
 cd datasets
 gdown --fuzzy "https://drive.google.com/file/d/1np57yAy4rxXTcoC217C115b7qI5At76D/view?usp=sharing"
+tar -xvf "VOS.tar"
+rm "VOS.tar"
 mkdir endovis2017
 cd endovis2017
 gdown --fuzzy "https://drive.google.com/file/d/1B7QYCG5JwtWTneq2WSqjPIbl04NQdEmv/view?usp=sharing"
 tar -xvf "eval_endovis2017_surgical.tar"
+rm "eval_endovis2017_surgical.tar"
 cd ..
 mkdir endovis2018
 cd endovis2018
 gdown --fuzzy "https://drive.google.com/file/d/1dL7s4iaC1JWxhxf3V3iBSr9cUxxZaaR8/view?usp=sharing"
 tar -xvf "eval_endovis18_surgical.tar"
-cd ..
+rm "eval_endovis18_surgical.tar"
+cd ../..
 
 cd checkpoints
 gdown --fuzzy "https://drive.google.com/file/d/1DyrrLKst1ZQwkgKM7BWCCwLxSXAgOcMI/view?usp=sharing"
